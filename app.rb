6bd216a313c6eb1835b8859975b38e8d3397d548
dilbani <claudiaenriquez241@gmail.com>
2022-09-05 16:49:01 -0500

    Creando Restaurante

--- app.rb
+++ app.rb
@@ -3,4 +3,51 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
+configure :development do
+  use Rack::MethodOverride # Habilitar los metodos delete y patch
+end
 
+get "/" do
+erb :home
+end
+
+get "/restaurants" do
+ @restaurants = Restaurant.all # toda la informacon del restaurante
+ erb :index # vista de la pagina
+end
+
+# para crear un restaurante
+get "/restaurants/new" do
+
+  erb :new
+end
+
+post "/restaurants" do #  boton guardar los datos nuevos
+Restaurant.create(name: params[:name],
+  city: params[:city],
+  image_url: params[:image_url],
+  phone_number: params[:phone_number],
+  address: params[:address],
+  type_food: params[:type_food])
+
+  redirect to "/restaurants"
+end
+
+# guardar ruta
+get "/restaurants/:id" do  # show
+  @restaurant = Restaurant.find(params[:id])
+  erb :show
+end
+
+# editar
+get "/restaurants/:id/edit" do # modificamos los datos
+
+end
+
+patch "/restaurants/:id" do # permite guardar los datos modificados Boton guardar datos modificados
+
+end
+
+delete "/restaurants/:id" do
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
+puts "Crenado restaurante"
 
+Restaurant.create(
+  name: "Pollo al dia",
+  address: "polaco av 32-40",
+  city: "Valenciana",
+  image_url: "https://topadventure.com/__export/1597001823905/sites/laverdad/img/2020/08/09/mejores_restaurantes.jpg_1209900187.jpg",
+  phone_number: "1453264",
+  type_food: "vino caliente"
+)
+
+Restaurant.create(
+  name: "menù al dìa",
+  address: "Praga av 60-40",
+  city: "Oregon",
+  image_url: "https://binaries.templates.cdn.office.net/support/templates/es-es/lt44813192_quantized.png",
+  phone_number: "3245679661",
+  type_food: "Todo menù"
+)
